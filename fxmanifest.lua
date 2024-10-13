d68a24b95fd5b8ccab69eb83a5c843be7be17f70
Tik0 <97636919+Tiik0@users.noreply.github.com>
2024-10-13 19:06:33 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'adamant'
+lua54 'yes'
+game 'gta5'
+author "Tik0"
+shared_script {
+    '@ox_lib/init.lua',
+    '@es_extended/imports.lua'
+}
+
+client_scripts {
+
+    'client.lua'
+}
+
+server_scripts { 
+}
+
+
